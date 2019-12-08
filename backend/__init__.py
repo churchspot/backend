10b6e1a2b1b7896219c19b322725ebf4d891b192
kamilmichna <kamil.michna28@gmail.com>
2019-12-08 19:21:34 +0100

    sucesfully added CORS

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from apis.weather_api import weather_api
 from apis.bible_api import bible_api
 from apis.yt_api import yt_api
+from flask_cors import CORS
 app = Flask(__name__)
 
 """
@@ -20,6 +21,8 @@ app = Flask(__name__)
 app.register_blueprint(weather_api, url_prefix='/api/weather/')
 app.register_blueprint(bible_api, url_prefix='/api/bible/')
 app.register_blueprint(yt_api, url_prefix='/api/yt/')
+CORS(app)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
