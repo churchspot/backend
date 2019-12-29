58c8f7905b93adc0f3e500ffc5a0f98cefc2faa3
kamilmichna <kamil.michna28@gmail.com>
2019-12-29 15:17:44 +0100

    added cache

--- apis/yt_api.py
+++ apis/yt_api.py
@@ -18,10 +18,12 @@ yt_api = Blueprint('yt_api', __name__)
 from config.api_keys import yt_api_key
 from config.yt_channels import yt_channels
 import json
+from config.cache import cache
 # https://www.googleapis.com/youtube/v3/search?key=AIzaSyCIhIfjiJD1jX7sI9g8OXNlFXIaA9aLliA&channelId=UC5KNKlhUr1Cy7fKuO4LRsaQ&part=snippet,id&order=date&maxResults=1
 
 
 @yt_api.route('/', methods = ["GET"])
+@cache.cached(timeout=3600)
 def get_newest_films():
     data = []
     for channel in yt_channels:
